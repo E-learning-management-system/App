e4bf2785261a5eedbaa281a434262786d97a29b7
ZGhanbari <z.ghanbari14@gmail.com>
2022-06-19 11:04:25 +0430

    student delivery

--- lib/views/tab_lessons/student_delivery_view.dart
+++ lib/views/tab_lessons/student_delivery_view.dart
@@ -5,15 +5,31 @@ import 'package:project/helpers/colors.dart';
 import 'package:project/helpers/constants.dart';
 import 'package:project/widgets/app_bar_widget.dart';
 import 'package:provider/provider.dart';
+
+import '../../widgets/empty_view_widget.dart';
 class StudentDeliveryView extends StatelessWidget {
-  const StudentDeliveryView({Key? key}) : super(key: key);
+  const StudentDeliveryView({Key? key,required this.title,required this.Id}) : super(key: key);
   static const String id = '/student_delivery';
+  final String title;
+  final int Id;
   @override
+
+
+
   Widget build(BuildContext context) {
     final isComplete = ModalRoute.of(context)!.settings.arguments as bool;
     final cnt = Provider.of<StudentDeliveryController>(context);
+    Future<List<String>?> list()async{
+     if(isComplete){
+       await cnt.getAnswerStudent(Id);
+       return cnt.answerStudent[Id];
+     }else{
+       await cnt.getNotAnswerStudent(Id);
+       return cnt.notAnswerStudent[Id];
+     }
+    }
     return Scaffold(
-      appBar: AppbarWidget(text: 'ریاضی2 - تکلیف 1',),
+      appBar: AppbarWidget(text: title,),
       body: Padding(
         padding: const EdgeInsets.only(
           top: 30
@@ -27,43 +43,56 @@ class StudentDeliveryView extends StatelessWidget {
               fontSize: 18
             ),),
             Expanded(
-                child: ListView.builder(
-                  padding: const EdgeInsets.only(top: 0,
-                  right: 15,left: 15),
-                  itemCount: cnt.listModel.length,
-                    itemExtent: 80,
-                    itemBuilder: (context, index) {
-                      final data = cnt.listModel[index];
-                      return Card(
-                        margin: const EdgeInsets.only(top: 20),
-                        color: MyColors.blueAccentHex,
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            top: 8,
-                            bottom: 8,
-                          ),
-                          child: Row(
-                            children: [
-                              Container(
-                                  margin: const EdgeInsets.only(right: 12),
-                                  child: Image.asset('$baseUrlImage/ic_profile.png',
-                                    height: 50,
-                                    width: 50,)),
-                              Container(
-                                margin: const EdgeInsets.only(right: 15),
-                                child: Text(data.title,
-                                    style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                                        color: Colors.white,
-                                      fontWeight: FontWeight.bold
-                                    )),
-                              ),
+                child: FutureBuilder(
+                  future: list(),
+                  builder:(context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (!snapshot.hasData) {
+                      return const EmptyViewWidget();
+                    }
+                    return ListView.builder(
+                      padding: const EdgeInsets.only(top: 0,
+                          right: 15,left: 15),
+                      itemCount:isComplete? cnt.answerStudent[Id]?.length: cnt.notAnswerStudent[Id]?.length,
+                      itemExtent: 80,
+                      itemBuilder: (context, index) {
+                        final data = isComplete? cnt.answerStudent[Id]![index]: cnt.notAnswerStudent[Id]![index];
+                        return Card(
+                          margin: const EdgeInsets.only(top: 20),
+                          color: MyColors.blueAccentHex,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              top: 8,
+                              bottom: 8,
+                            ),
+                            child: Row(
+                              children: [
+                                Container(
+                                    margin: const EdgeInsets.only(right: 12),
+                                    child: Image.asset('$baseUrlImage/ic_profile.png',
+                                      height: 50,
+                                      width: 50,)),
+                                Container(
+                                  margin: const EdgeInsets.only(right: 15),
+                                  child: Text(data,
+                                      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold
+                                      )),
+                                ),
 
-                            ],
+                              ],
+                            ),
                           ),
-                        ),
-                      );
-                    },))
+                        );
+                      },
+                    );
+                  },
+                ),  )
+
 
           ],
         ),
